abdba61d8ffe3d8bfb637046f9c529d1366e7327
Willa Shiomos <willashiomos@Willas-MacBook-Air.local>
2020-11-12 21:03:27 -0500

    rest of pages + new images

--- js/main.js
+++ js/main.js
@@ -5,43 +5,35 @@ var resizeThreshold = 799;
 var priorW = $(window).width();
 var loadW = priorW;
 
-
-
-
-var loadEvents = function () {
-	
-	
-	
-
-}
-
+var loadEvents = function () {};
 
 function loadMoreEmployee() {
-	$(".yt-video-container a").click(function (e) {
-		var videoURL = $(this).attr("href");
-		if (videoURL.indexOf('?') == -1) {
-			videoURL += "?";
-		}
-		if (videoURL.indexOf('autoplay') == -1) {
-			videoURL += "&autoplay=1";
-		}
-		if (videoURL.indexOf('rel') == -1) {
-			videoURL += "&rel=0";
-		}
-		$(this).parent().html('<iframe width="560" title="Youtube video -' + $(this).attr("href") + ' " height="315" src="' + videoURL + '" frameborder="0" allowfullscreen=""></iframe>');
-		e.preventDefault();
-		$("#video-player iframe").attr("src", $(this).attr("href"));
-	});
-	
+  $(".yt-video-container a").click(function (e) {
+    var videoURL = $(this).attr("href");
+    if (videoURL.indexOf("?") == -1) {
+      videoURL += "?";
+    }
+    if (videoURL.indexOf("autoplay") == -1) {
+      videoURL += "&autoplay=1";
+    }
+    if (videoURL.indexOf("rel") == -1) {
+      videoURL += "&rel=0";
+    }
+    $(this)
+      .parent()
+      .html(
+        '<iframe width="560" title="Youtube video -' +
+          $(this).attr("href") +
+          ' " height="315" src="' +
+          videoURL +
+          '" frameborder="0" allowfullscreen=""></iframe>'
+      );
+    e.preventDefault();
+    $("#video-player iframe").attr("src", $(this).attr("href"));
+  });
 }
-
-
-
-
 
 // document ready load events
 $(function () {
-	loadEvents();
+  loadEvents();
 });
-
-
